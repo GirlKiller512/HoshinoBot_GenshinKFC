82e6c98a45f256c1f6d4efef67df9abaf963f45a
Liza Good <GirlKiller512@users.noreply.github.com>
2021-03-13 20:06:39 +0800

    Update genshinkfc.py

--- genshinkfc/genshinkfc.py
+++ genshinkfc/genshinkfc.py
@@ -1,15 +1,22 @@
 import os
+import random
 from nonebot.exceptions import CQHttpError
 from nonebot import MessageSegment
-from hoshino import R, Service, priv
+from hoshino import Service
 
 sv = Service('genshinkfc', enable_on_default=True, visible=False)
+
+
+def get_kfc():
+    files = os.listdir(os.path.dirname(__file__) + '/record')
+    rec = random.choice(files)
+    return rec
 
 
 @sv.on_fullmatch(('KFC', 'kfc', '原神KFC', '原神kfc', '二次元KFC', '二次元kfc', '异世相遇尽享美味'))
 async def kfc(bot, ev) -> MessageSegment:
     try:
-        rec = MessageSegment.record(f'file:///{os.path.dirname(__file__)}/genshinkfc.mp3')
+        rec = MessageSegment.record(f'file:///{os.path.dirname(__file__)}/record/{get_kfc()}')
         await bot.send(ev, rec)
     except CQHttpError:
         sv.logger.error("发送失败")
